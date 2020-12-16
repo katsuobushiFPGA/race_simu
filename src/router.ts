f9c9cfa573c6ba26c436ec0d9c38cd44fc493807
horiba <suizlet@gmail.com>
2020-12-16 22:11:47 +0900

    [feat] add vuetify

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Top from './components/Top.vue';
+import HelloWorld from './components/HelloWorld.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
     { path: '/', component: Top },
+    { path: '/home', component: HelloWorld },
 ];
 
 const router = new VueRouter({
